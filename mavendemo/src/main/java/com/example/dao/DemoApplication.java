30b340c92778b844ed64c70476356db85814dfba
wjjing <546076628@qq.com>
2018-10-11 16:59:01 +0800

    Controller层完成

--- mavendemo/src/main/java/com/example/dao/DemoApplication.java
+++ mavendemo/src/main/java/com/example/dao/DemoApplication.java
@@ -2,10 +2,8 @@ package com.example.dao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class DemoApplication {
 
 	
